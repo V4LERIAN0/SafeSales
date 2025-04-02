802f56410f8be2cbd496ca869fa95f8ddf5089f7
EdwinNM22 <edwin.nunez@catolica.edu.sv>
2025-04-02 09:15:54 -0600

    Update

--- src/main/java/sv/edu/catolica/safesales/service/ProductoService.java
+++ src/main/java/sv/edu/catolica/safesales/service/ProductoService.java
@@ -1,10 +1,12 @@
 package sv.edu.catolica.safesales.service;
 
+import org.springframework.stereotype.Service;
 import sv.edu.catolica.safesales.entities.ProductoEntity;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public interface ProductoService {
 
     public ProductoEntity save( ProductoEntity producto);
--- src/main/java/sv/edu/catolica/safesales/service/TiendaService.java
+++ src/main/java/sv/edu/catolica/safesales/service/TiendaService.java
@@ -1,9 +1,11 @@
 package sv.edu.catolica.safesales.service;
 
+import org.springframework.stereotype.Service;
 import sv.edu.catolica.safesales.entities.TiendaEntity;
 
 import java.util.List;
 
+@Service
 public interface TiendaService {
 
     List<TiendaEntity> findAll();
